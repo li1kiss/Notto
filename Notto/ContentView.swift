aa110475cc5b9338b7ccf0947f892609d95b3f1b
Mykhailo Kravchuk <mishakr232@gmail.com>
2024-10-17 20:39:01 +0200

    Initial Commit

---
+++ Notto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notto
+//
+//  Created by Mykhailo Kravchuk on 17/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notto/NottoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NottoApp.swift
+//  Notto
+//
+//  Created by Mykhailo Kravchuk on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct NottoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
